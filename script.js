a119dd7675e82ac90e250966d4953371ccce16a2
Gabester0 <gceipper@gmail.com>
2018-08-15 23:04:19 -0400

    day 5.  Forgot to make new branch for this day

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const form = document.getElementById("survey-form");
 const label = document.querySelectorAll("label");
 const dropDown = document.getElementById("dropdown");
+const number = document.getElementById("number");
+
+form.addEventListener("focusin", e => {
+    label.forEach(l => l.style.color = "black");
+    dropDown.style.color = "black";
+})
 
 form.addEventListener('mouseenter', e => {
     label.forEach(l => l.style.color = "black");
@@ -12,7 +18,19 @@ form.addEventListener('mouseleave', e=>{
     dropDown.style.color = "white";
 });
 
+
 dropDown.addEventListener("change", e =>{ 
     var sel = dropDown.value;
-    alert(sel);
+    updateNumberFormat(sel);
 });
+
+function updateNumberFormat(sel){
+    if(sel === "mexico"){
+        number.pattern = "[0-9]{2,3}-[0-9]{4}-[0-9]{4}";
+        number.placeholder="00-0000-0000 or 000-0000-0000";
+    } 
+    if(sel === "us"){
+        number.pattern = "[0-9]{3}-[0-9]{3}-[0-9]{4}";
+        number.placeholder="000-000-0000";
+    }
+}
